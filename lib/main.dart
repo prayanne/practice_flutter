82013fbbc2bc45dfbc9ab253be72b207304fc34a
prayanne <kyhy0321@gmail.com>
2026-01-11 11:44:55 +0900

    convert stateful

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,14 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,9 +80,11 @@ class MyHomePage extends StatelessWidget {
                   label: Text('Favorite'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             ),
           ),
